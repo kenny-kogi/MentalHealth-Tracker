6ff3dc0ab6be6814957327a896657e4279aa56ae
kenny-kogi <mwangiken497@gmail.com>
2022-01-10 16:17:51 +0300

    setting up user authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = User.create([{
+    firstName: "kenneth",
+    lastName: "mwangi",
+    username: "kenny",
+    email: "mwangiken497@gmail.com",
+    password: "test",
+    password_confirmation: "test",
+    location: "Nairobi",
+    age: 18,
+    occupation: "coder",
+    gender: "male"
+
+}])
+
+
+ 

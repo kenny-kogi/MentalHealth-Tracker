b708657fb2a617a9c995c340ce49d2c4cfbe2be9
kenny-kogi <mwangiken497@gmail.com>
2022-01-15 19:01:00 +0300

    moods model initial setup and commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :moods, dependent: :destroy
+
+
 
     validates :username, presence: true 
     validates :username, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_10_101800) do
+ActiveRecord::Schema.define(version: 2022_01_15_150837) do
+
+  create_table "moods", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "hours_slept"
+    t.string "depressed"
+    t.string "anxiety"
+    t.integer "irritability_level"
+    t.integer "elevated_level"
+    t.boolean "psychotic_symptoms"
+    t.string "weather"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_moods_on_user_id"
+  end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "firstName"
@@ -27,4 +41,5 @@ ActiveRecord::Schema.define(version: 2022_01_10_101800) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "moods", "users"
 end

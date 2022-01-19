f1d0f163f3f817f287c6c2696d96eb362ea6f584
kenny-kogi <mwangiken497@gmail.com>
2022-01-19 18:12:57 +0300

    sign up:page working

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
     validates :email, presence: true
     validates :email, uniqueness: true
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-    validates :firstName, presence: true
-    validates :lastName, presence: true 
+    # validates :firstName, presence: true
+    # validates :lastName, presence: true 
 end

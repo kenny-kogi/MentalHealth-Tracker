0b2b2ac6f667a84e63ae21e1b6a87b63ec416ca6
kenny-kogi <mwangiken497@gmail.com>
2022-02-19 22:31:28 +0300

    user.rb model changes/get hours slept data

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,16 +13,24 @@ class User < ApplicationRecord
     # validates :lastName, presence: true 
 
     def self.getslepthoursdata(id)
+        # hours_slept_array = []
+        # hours_slept_created_date = []
         hours_slept_array = []
-        hours_slept_created_date = []
         sleepinghoursdata = User.find_by(id: id).moods.select("hours_slept", "created_at")
-        sleepinghoursdata.map{  |data|  hours_slept_array.push(data.hours_slept)
-                                        hours_slept_created_date.push(data.created_at)
+        sleepinghoursdata.map{  |data|  
+            
+            # hours_slept_array.push(data.hours_slept)
+                                        
+            # hours_slept_created_date.push(data.created_at)
+
+            hours_slept_array.push({x: data.created_at, y: data.hours_slept})
         }
                      
-        {  hours_slept:  hours_slept_array.to_a,
-           date_created: hours_slept_created_date.to_a
-        }
+        # {  hours_slept:  hours_slept_array.to_a,
+        #    date_created: hours_slept_created_date.to_a
+        # }
+
+        hours_slept_array
     end
 
 end
